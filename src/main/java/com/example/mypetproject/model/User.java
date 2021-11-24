b75ff46b5d265d9ce90a5aa962cd6099ebb64690
alexandr.porotov@yandex.ru <xDv_3t_o_pr>
2021-11-24 23:57:26 +0300

    added user entity

--- src/main/java/com/example/mypetproject/model/User.java
+++ src/main/java/com/example/mypetproject/model/User.java
@@ -1,4 +1,29 @@
 package com.example.mypetproject.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Data
 public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_mail")
+    private String userMail;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @Column(name = "user_balance")
+    private Long userBalance;
+
 }
